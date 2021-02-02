1948add4b1e4edd58492b956a2ebe8c5ebf8474e
AhyeonKim <kah0396@naver.com>
2021-02-02 18:02:25 +0900

    DP

---
+++ BOJ/boj_2156_포도주시식.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+step = []
+for _ in range(n):
+    step.append(int(input()))
+dp = [step[0]]
+for i in range(1,n):
+    one = dp[i-2]+step[i]
+    two = step[i-1]+step[i]
+    if i-3>-1:
+        two+=dp[i-3]
+    dp.append(max(one,two,dp[i-1]))
+print(max(dp))
