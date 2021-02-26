69fb8bfdc1dbc0968f59768cff810f6bacd71c3e
AhyeonKim <kah0396@naver.com>
2021-02-27 01:16:01 +0900

    시뮬레이션

--- BOJ/boj_3190_뱀.py
+++ BOJ/boj_3190_뱀.py
@@ -4,12 +4,12 @@ def isIn(i,j):
 input=sys.stdin.readline
 N = int(input())
 K = int(input())
-move = [[0,-1],[-1,0],[0,1],[1,0]] # L(0),U(1),R(2),D(3)
+move = [[0,-1],[-1,0],[0,1],[1,0]]
 board = [[-1]*N for _ in range(N)]
 board[0][0]=2
 for _ in range(K):
     i,j = map(int,input().split())
-    board[i][j]=4 # 사과
+    board[i-1][j-1]=4 # 사과
 L = int(input())
 info=[]
 for _ in range(L):
@@ -18,29 +18,27 @@ for _ in range(L):
 head=[0,0]
 tail=[0,0]
 t=0
-while True:
+flag = True
+while flag:
+    t+=1
     d = board[head[0]][head[1]]
-    if info and info[0][0]==t:
-        tmp = info[0][1]
-        info.pop(0)
-        if tmp=='L':
-            d=0
-        elif tmp=='U':
-            d=1
-        elif tmp=='R':
-            d=2
-        elif tmp=='D':
-            d=3
-
     ni,nj = head[0]+move[d][0], head[1]+move[d][1]
-    
     if not isIn(ni,nj) or -1<board[ni][nj]<4:
         break
     if board[ni][nj]!=4:
         td = board[tail[0]][tail[1]]
         board[tail[0]][tail[1]]=-1
-        tail[0],tail[1] = tail[0]+move[td][0], tail[1]+move[td][1]
+        tail = [tail[0]+move[td][0], tail[1]+move[td][1]]
     head = [ni,nj]
     board[ni][nj] = d
-    t+=1
+    if info and info[0][0]==t:
+        tmp = info[0][1]
+        info.pop(0)
+        if tmp=='L':
+            d=d-1
+            if d<0:
+                d=3
+        elif tmp=='D':
+            d=(d+1)%4
+    board[head[0]][head[1]]=d
 print(t)
