03ea370b8b65c0ed26cf38dd3d831565e5b140e5
AhyeonKim <kah0396@naver.com>
2021-03-02 23:52:47 +0900

    210302

---
+++ BOJ/boj_14499_주사위굴리기.py
@@ -0,0 +1,7 @@
+import sys
+input = sys.stdin.readline
+N,M,x,y,K = map(int,input().split())
+board = []
+for _ in range(N):
+    board.append(list(map(int,input().split())))
+move = list(map(int,input().split()))
