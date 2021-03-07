6ea3473457be1ade0dfa35d61560c3c628c7fac0
AhyeonKim <kah0396@naver.com>
2021-03-07 23:25:26 +0900

    DP

--- BOJ/boj_1915_가장큰정사각형.py
+++ BOJ/boj_1915_가장큰정사각형.py
@@ -1,25 +1,23 @@
 import sys
 input = sys.stdin.readline
-def check(i,j,m):
-    size = m
-    while True:
-        tmp = size+1
-        if i+tmp<=n and j+tmp<=m:
-            for k1 in range(tmp):
-                ni = i+k1
-                for k2 in range(tmp):
-                    nj = j+k2
-                    if board[ni][nj]!=1:
-                        return size
-        else: return size
-        size = tmp
 n,m = map(int,input().split())
 board = []
 for _ in range(n):
     board.append(list(map(int,input().strip())))
 max_size = 0
-for i in range(n):
-    for j in range(m):
-        if board[i][j]==1:
-            max_size = check(i,j,max_size)
+for j in range(m):
+    if board[0][j]==1:
+        max_size=1
+        break
+if max_size==0:
+    for i in range(n):
+        if board[i][0]==1:
+            max_size=1
+            break
+for i in range(1,n):
+    for j in range(1,m):
+        if board[i][j]!=0:
+            tmp = min(board[i-1][j-1], board[i-1][j], board[i][j-1])
+            board[i][j] += tmp
+            max_size = max(max_size,board[i][j])
 print(max_size*max_size)
