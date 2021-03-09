3a1f1ffb8bb726d5d286ea20281f6d11f073f43a
AhyeonKim <kah0396@naver.com>
2021-03-10 00:39:38 +0900

    구현

--- BOJ/boj_20055_컨베이어벨트위의로봇.py
+++ BOJ/boj_20055_컨베이어벨트위의로봇.py
@@ -1,29 +1,25 @@
 N,K = map(int,input().split())
 belt = list(map(int,input().split()))
-robot = [False]*(2*N)
+robot = [False]*N
 step=0
+cnt=0
 while True:
     step+=1
-    tmp = belt[N-1]
+    tmp = belt[len(belt)-1]
     belt = [tmp]+belt[:-1]
     robot = [False]+robot[:-1]
-    if belt[0]>0:
-        robot[0]=True
-        belt[0]-=1
-        robot[N-1]=False
-        for i in range(N-2,-1,-1):
-            if robot[i] and belt[i+1]>0 and not robot[i+1]:
-                robot[i+1]=True
-                robot[i]=False
-                belt[i+1]-=1
-            else:
-                break
+    robot[N-1]=False
+    for i in range(N-2,-1,-1):
+        if robot[i] and belt[i+1]>0 and not robot[i+1]:
+            robot[i+1]=True
+            robot[i]=False
+            belt[i+1]-=1
+            if belt[i+1]==0:
+                cnt+=1
     if belt[0]>0 and not robot[0]:
         robot[0]=True
         belt[0]-=1
-    cnt=0
-    for i in belt:
-        if i==0:
+        if belt[0]==0:
             cnt+=1
     if cnt>=K:
         break
