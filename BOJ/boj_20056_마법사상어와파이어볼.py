f3b0fde11c229d5592f678124556b9aca002d01f
AhyeonKim <kah0396@naver.com>
2021-03-24 00:18:51 +0900

    시뮬레이션

--- BOJ/boj_20056_마법사상어와파이어볼.py
+++ BOJ/boj_20056_마법사상어와파이어볼.py
@@ -4,33 +4,42 @@ board = [[[] for _ in range(N)] for _ in range(N)]
 for _ in range(M):
     r,c,m,s,d = map(int,input().split())
     board[r-1][c-1].append((m,s,d))
+for _ in range(K):
+    tmp = {}
+    for i in range(N):
+        for j in range(N):
+            if board[i][j]:
+                for m,s,d in board[i][j]:
+                    ni, nj = (i+di[d]*s)%N, (j+dj[d]*s)%N
+                    if (ni,nj) in tmp:
+                        tmp[(ni,nj)].append((m,s,d))
+                    else:
+                        tmp[(ni,nj)]=[(m,s,d)]
+                board[i][j]=[]
 
-#move
-tmp = {}
+    for i,j in tmp:
+        balls = tmp[(i,j)]
+        if len(balls)>1:
+            total_m, total_s, total_d = 0,0,0
+            for m,s,d in balls:
+                total_m+=m
+                total_s+=s
+                total_d+=d%2
+            total_m//=5
+            total_s//=len(balls)
+            if total_m:
+                for k in range(4):
+                    if total_d in [0,len(balls)]:
+                        d = 2*k
+                    else:
+                        d = 2*k+1
+                    board[i][j].append((total_m,total_s,d))
+        else:
+            board[i][j]=balls
+ans = 0
 for i in range(N):
     for j in range(N):
         if board[i][j]:
             for m,s,d in board[i][j]:
-                ni, nj = (i+di[d]*s)%N, (j+dj[d]*s)%N
-                if (ni,nj) in tmp:
-                    tmp[(ni,nj)].append((m,s,d))
-                else:
-                    tmp[(ni,nj)]=[(m,s,d)]
-print(tmp)
-
-#crush - sofa
-for i,j in tmp:
-    balls = tmp[(i,j)]
-    if len(balls)>1:
-        tmp[(i,j)]=[]
-        total_m, total_s, total_d = 0,0,0
-        for m,s,d in balls:
-            total_m+=m
-            total_s+=s
-            total_d+=d%2
-        total_m//=5
-        total_s//=len(balls)
-        for k in range(4):
-            if total_d in [0,len(balls)]:
-                
-
+                ans+=m
+print(ans)
