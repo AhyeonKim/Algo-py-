93971fc4356cb29703ef60887e8647e41a5cb578
AhyeonKim <kah0396@naver.com>
2021-04-07 21:53:45 +0900

    구현

--- BOJ/boj_14890_경사로.py
+++ BOJ/boj_14890_경사로.py
@@ -1,10 +1,44 @@
-N,L = map(int,input().split()) # ['6','2']
-print(N,L)
-
-# board = [list(map(int,input().split())) for _ in range(N)]
-# print(board)
-#list('3 3 3 3 3 3') = [ '3',' ','3']
-board = []
-for _ in range(N):
-    board.append(list(map(int,input().split())))
-print(board)
+def check(line):
+    global ans
+    cur = line[0]
+    visit = [False for _ in range(N)]
+    i = 0
+    while(i<N):
+        if cur==line[i]:
+            i+=1
+            continue
+        else:
+            if abs(cur-line[i])!=1:
+                return
+            else:
+                if cur-line[i]==1: #낮아질 때
+                    for j in range(L):
+                        ni = i+j
+                        if ni<N and cur-line[ni]==1 and not visit[ni]:
+                            visit[ni] = True
+                        else:
+                            return
+                    cur=line[i]
+                    i+=L
+                elif cur-line[i]==-1: #높아질 때
+                    for j in range(1,L+1):
+                        ni = i-j
+                        if -1<ni and cur==line[ni] and not visit[ni]:
+                            visit[ni] = True
+                        else:
+                            return
+                    cur=line[i]
+                    i+=1
+    ans+=1
+    return
+N,L = map(int,input().split())
+board = [list(map(int,input().split())) for _ in range(N)]
+ans = 0
+for i in board:
+    check(i)
+for j in range(N):
+    tmp = []
+    for i in range(N):
+        tmp.append(board[i][j])
+    check(tmp)
+print(ans)
