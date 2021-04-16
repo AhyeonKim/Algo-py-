53fb07a227ed393b95ec4989a9843b371b90d4e1
AhyeonKim <kah0396@naver.com>
2021-04-16 23:56:54 +0900

    210416

--- BOJ/boj_15683_감시.py
+++ BOJ/boj_15683_감시.py
@@ -2,3 +2,7 @@ import sys
 input = sys.stdin.readline
 def isIn(i,j):
     return -1<i<N and -1<j<N
+N,M = map(int,input().split())
+board = [list(map(int,input().split())) for _ in range(N)]
+answer = 0
+print(board)
