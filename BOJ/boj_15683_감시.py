fd9c4e25168ce248e12ae9cc05f3057677f1b386
AhyeonKim <kah0396@naver.com>
2021-04-16 23:52:04 +0900

    210416

--- BOJ/boj_15683_감시.py
+++ BOJ/boj_15683_감시.py
@@ -2,4 +2,3 @@ import sys
 input = sys.stdin.readline
 def isIn(i,j):
     return -1<i<N and -1<j<N
-cctv = {1:[(0,1)],2:[(0,-1),(0,1)],3:[(-1,0),(0,1)],4:[(0,-1),(-1,0),(0,1)],5:[(0,-1),(-1,0),(0,1),(1,0)]}
