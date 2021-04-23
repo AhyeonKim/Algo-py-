46b29bcd1bac2b29932224e16dbf48b698e075b4
AhyeonKim <kah0396@naver.com>
2021-04-23 23:23:01 +0900

    시뮬레이션

--- BOJ/boj_20056_마법사상어와파이어볼.py
+++ BOJ/boj_20056_마법사상어와파이어볼.py
@@ -1,45 +1,47 @@
-di,dj = [-1,-1,0,1,1,1,0,-1],[0,1,1,1,0,-1,-1,-1]
+di, dj = [-1,-1,0,1,1,1,0,-1], [0,1,1,1,0,-1,-1,-1]
 N,M,K = map(int,input().split())
-board = [[[] for _ in range(N)] for _ in range(N)]
+balls = []
 for _ in range(M):
     r,c,m,s,d = map(int,input().split())
-    board[r-1][c-1].append((m,s,d))
-for _ in range(K):
+    balls.append((r,c,m,s,d))
+move = 0
+while move<K:
+    move+=1
     tmp = {}
-    for i in range(N):
-        for j in range(N):
-            if board[i][j]:
-                for m,s,d in board[i][j]:
-                    ni, nj = (i+di[d]*s)%N, (j+dj[d]*s)%N
-                    if (ni,nj) in tmp:
-                        tmp[(ni,nj)].append((m,s,d))
-                    else:
-                        tmp[(ni,nj)]=[(m,s,d)]
-                board[i][j]=[]
-
-    for i,j in tmp:
-        balls = tmp[(i,j)]
-        if len(balls)>1:
-            total_m, total_s, total_d = 0,0,0
-            for m,s,d in balls:
-                total_m+=m
-                total_s+=s
-                total_d+=d%2
-            total_m//=5
-            total_s//=len(balls)
-            if total_m:
-                for k in range(4):
-                    if total_d in [0,len(balls)]:
-                        d = 2*k
-                    else:
-                        d = 2*k+1
-                    board[i][j].append((total_m,total_s,d))
+    for ball in balls:
+        r,c,m,s,d = ball
+        nr,nc = (r+di[d]*s)%N, (c+dj[d]*s)%N
+        if (nr,nc) in tmp:
+            tmp[(nr,nc)].append((m,s,d))
         else:
-            board[i][j]=balls
-ans = 0
-for i in range(N):
-    for j in range(N):
-        if board[i][j]:
-            for m,s,d in board[i][j]:
-                ans+=m
-print(ans)
+            tmp[(nr,nc)]=[(m,s,d)]
+    balls=[]
+    for r,c in tmp:
+        if len(tmp[(r,c)])>1:
+            length = len(tmp[(r,c)])
+            m_sum=0
+            s_sum=0
+            d_sum=0
+            for t in tmp[(r,c)]:
+                m,s,d = t
+                m_sum+=m
+                s_sum+=s
+                d_sum+=d%2
+            new_m = m_sum//5
+            if new_m>0:
+                new_s = s_sum//length
+                new_d = []
+                if d_sum==0 or d_sum==length:
+                    new_d = [0,2,4,6]
+                else:
+                    new_d = [1,3,5,7]
+                for d in new_d:
+                    balls.append((r,c,new_m,new_s,d))
+        else:
+            m,s,d = tmp[(r,c)][0]
+            balls.append((r,c,m,s,d))
+answer = 0
+for ball in balls:
+    r,c,m,s,d = ball
+    answer+=m
+print(answer)
